9c48984e6a73ebde6b40f693f3fd079913d5568e
haruka1130 <lesjours1130@gmail.com>
2021-02-17 22:18:03 +0900

    修正

--- diary_app/config/routes.rb
+++ diary_app/config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   post 'posts/create'=>"posts#create"
   post 'posts/:id/update'=>'posts#update'
 
-  get '/'=>"home#top"
+  root "home#top"
   get "/about"=>"home#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
